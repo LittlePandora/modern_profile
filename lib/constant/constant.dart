2b0709500720cf40d62819b6db220cb35e4ac065
LittlePandora <122350414+LittlePandora@users.noreply.github.com>
2024-09-09 13:27:23 +0700

    Add files via upload

---
+++ lib/constant/constant.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const bgPrimaryColor = Colors.black;
+const bgSecondaryColor = Color.fromRGBO(191, 191, 191, 1);
+
+const textPrimaryColor = Colors.white;
+const textSecondaryColor = Colors.black;
+
+const iconPrimaryColor = Color.fromARGB(255, 126, 255, 221);
+const iconSecondaryColor = Colors.white;
+
+final textTitle = GoogleFonts.lato(
+  fontSize: 20,
+  color: textPrimaryColor,
+  fontWeight: FontWeight.w600,
+);
+
+final textSubTitle = GoogleFonts.lato(
+  fontSize: 14,
+  color: Colors.grey,
+  fontWeight: FontWeight.w400,
+);
+
+final textBtn = GoogleFonts.lato(
+  fontSize: 18,
+  color: textSecondaryColor,
+  fontWeight: FontWeight.w400,
+);
+
+final textMenu = GoogleFonts.lato(
+  fontSize: 16,
+  color: textPrimaryColor,
+  fontWeight: FontWeight.w400,
+);
+
